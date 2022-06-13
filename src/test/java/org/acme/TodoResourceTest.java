2418edf922fe84f1532fd427ba305e73bb0ddd0b
Andy Damevin <ia3andy@gmail.com>
2022-06-13 16:50:19 +0200

    Add web-app and wiring

--- src/test/java/org/acme/TodoResourceTest.java
+++ src/test/java/org/acme/TodoResourceTest.java
@@ -1,21 +1,22 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+public class TodoResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testTodoEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/api/todo")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is("[]"));
     }
 
 }
